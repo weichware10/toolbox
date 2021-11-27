674d0052a481ce1102718cc8e296b6a1e3243ec1
joshuajeschek <joshua.jeschek@gmail.com>
2021-11-27 14:07:17 +0100

    🌱 initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+![test status](.github/badges/tests.svg) ![code coverage](.github/badges/jacoco.svg)
+# Weichware-10: Toolbox
+
+Beinhaltet Toolbox
---
+++ src/main/java/com/github/weichware10/toolbox/App.java
@@ -0,0 +1,10 @@
+package com.github.weichware10.toolbox;
+
+/**
+ * Hello world.
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
