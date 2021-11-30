4b251226d67b54deb41e2bda385dbf857e315d08
grenoui11e <92694715+grenoui11e@users.noreply.github.com>
2021-11-30 23:58:54 +0100

    test-codecharts (#2)
    * anfang tests & strucktur
    
    * Grundstrucktur CodeCharts
    
    * weitere Bearbeitung CodeCharts Grundstruktur
    
    * tests CodeCharts erstellt
    
    * Grundstruktur verbessert und Tests angefangen
    
    * kleine Korrekturen
    
    * Fertigstellung Test & Verbesserung der Module
    
    * Update CodeChartsCoordinatorTest.java
    
    * Fehlerkorrektur Einagbef.
    
    * fixed build
    
    * mini Änderung
    
    Co-authored-by: joshuajeschek <joshua.jeschek@gmail.com>

--- src/main/java/github/weichware10/toolbox/codecharts/CodeChartsCoordinator.java
+++ src/main/java/github/weichware10/toolbox/codecharts/CodeChartsCoordinator.java
@@ -1,8 +1,81 @@
 package github.weichware10.toolbox.codecharts;
 
+import github.weichware10.util.Data;
+
 /**
- * TODO: David.
+ * Steuert den Datenverkehr und gibt die BEfehle zum anzeigen.
+ * von Bild Raster und Eingabefenster.
  */
 public class CodeChartsCoordinator {
+    protected Data data;
+    // private long[] speed = new long[2];
+    private static boolean isRelative;
+    private static int[] dimensions = new int[2];
+    private static String[] strings;
+    private boolean configData;
 
+    public int[] getDimensions() {
+        return dimensions;
+    }
+
+    public String[] getStrings() {
+        return strings;
+    }
+
+    public boolean getIsRelative() {
+        return isRelative;
+    }
+
+    /**
+     * Ruft alle Klassen und Funktionen in Reihenfolge auf in der CodeCharts durchgeführt wird.
+     * Holt Daten aus der Config
+     */
+    public void startCodeCharts() {
+        loadConfigData();
+        if (configData == false) {
+            errorMessage();
+        }
+        // CodeChartsBild bild = new CodeChartsBild("location");
+        // Speichert location in Data für Berechnung in Raster
+        // CodeChartsRaster raster = new CodeChartsRaster();
+        // CodeChartsEingabefenster fenster = new CodeChartsEingabefenster();
+        // bild.show();
+        // wait(speed[0]);
+        // bild.hide();
+        // raster.show();
+        // wait(speed[1]);
+        // String string = fenster.show();
+        // raster.setInput(string);
+        // ...
+        // raster.sendData();
+    }
+
+    /**
+     * Gibt Fehlermeldung bei falscher Config data.
+     */
+    private void errorMessage() {
+        ;
+    }
+
+    /**
+     * Holt die Daten aus der Config.
+     * Speichert die Daten.
+     *
+     * @return gibt zurück ob fertig geladen.
+     */
+    protected boolean loadConfigData() {
+        // if (data != vollständig) {
+        //     configData = false;
+        //     return configData;
+        // } else {
+        //     configData = true;
+        // }
+        // speed => Umrechnung von float zu long
+        // speed[0] = aus Config;
+        // speed[1] = aus Config;
+        // dimensions[0] = aus Config;
+        // dimensions[1] = aus Config;
+        // isRelative = aus Cofig;
+        return configData;
+    }
 }
