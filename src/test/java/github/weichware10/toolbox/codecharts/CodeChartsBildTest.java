1baeea54f3768c6e3540bc6dbc0c0183ced71752
grenoui11e <doehme3@gmail.com>
2021-11-30 17:13:52 +0100

    tests CodeCharts erstellt

---
+++ src/test/java/github/weichware10/toolbox/codecharts/CodeChartsBildTest.java
@@ -0,0 +1,18 @@
+package github.weichware10.toolbox.codecharts;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ * TODO: David.
+ */
+public class CodeChartsBildTest {
+    /**
+     * Testet ob die URL richtig ist.
+     */
+    @Ignore
+    @Test
+    public boolean correctUrl() {
+        return false;
+    }
+}
