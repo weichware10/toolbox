d7998bfbc2f4b704ccd72b6d0103d73d4c424787
joshuajeschek <joshua.jeschek@gmail.com>
2021-12-09 14:50:58 +0100

    [feat] Platzhalter Fenster hinzugefügt

---
+++ src/main/java/github/weichware10/toolbox/Main.java
@@ -0,0 +1,31 @@
+package github.weichware10.toolbox;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+/**
+ * Hello world.
+ *
+ */
+public class Main extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Title of the Window");
+        Button button = new Button();
+        button.setText("Click me");
+
+        StackPane layout = new StackPane();
+        layout.getChildren().add(button);
+
+        Scene scene = new Scene(layout, 300, 250);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+}
