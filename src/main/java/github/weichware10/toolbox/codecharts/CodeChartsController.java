e6c2256857b1bab5deea59bada5ad276fe21c83c
justinexner <92640370+justinexner@users.noreply.github.com>
2022-01-06 16:53:31 +0100

    [refactor] Grid auf Image

--- src/main/java/github/weichware10/toolbox/codecharts/CodeChartsController.java
+++ src/main/java/github/weichware10/toolbox/codecharts/CodeChartsController.java
@@ -5,8 +5,11 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.beans.value.ObservableDoubleValue;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.Pane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
 
 /**
  * Controller for {@link CodeCharts}.
@@ -18,9 +21,13 @@ public class CodeChartsController {
     @FXML
     private URL location;
     @FXML
+    private BorderPane codeChartsBorderPane;
+    @FXML
     private ImageView codeChartsImageView;
     @FXML
-    private Pane codeChartsPane;
+    private StackPane codeChartsStackPane;
+    @FXML
+    private GridPane codeChartsGridPane;
 
     private CodeCharts codeCharts;
 
@@ -36,21 +43,33 @@ public class CodeChartsController {
 
     private void setImageViewWidth(ObservableDoubleValue width) {
         codeChartsImageView.setFitWidth(width.get());
+        codeChartsGridPane.setPrefWidth(width.get());
     }
 
     private void setImageViewHeight(ObservableDoubleValue height) {
         codeChartsImageView.setFitHeight(height.get());
+        codeChartsGridPane.setPrefHeight(height.get());
+    }
+
+    private void setGridPane(){
+        codeChartsGridPane.add(new TextField("Test"), 0, 0);
+        codeChartsGridPane.add(new TextField("Test"), 3, 3);
     }
 
     @FXML
     void initialize() {
-        assert codeChartsImageView != null
-                : "fx:id=\"codeChartsImageView\" not injected: check 'CodeCharts.fxml'.";
-        assert codeChartsPane != null
-                : "fx:id=\"codeChartsPane\" not injected: check 'CodeCharts.fxml'.";
-        codeChartsPane.widthProperty().addListener(
+        assert codeChartsBorderPane != null :
+        "fx:id=\"codeChartsBorderPane\" was not injected: check your FXML file 'CodeCharts.fxml'.";
+        assert codeChartsGridPane != null :
+        "fx:id=\"codeChartsGridPane\" was not injected: check your FXML file 'CodeCharts.fxml'.";
+        assert codeChartsImageView != null :
+        "fx:id=\"codeChartsImageView\" was not injected: check your FXML file 'CodeCharts.fxml'.";
+        assert codeChartsStackPane != null :
+        "fx:id=\"codeChartsStackPane\" was not injected: check your FXML file 'CodeCharts.fxml'.";
+        codeChartsBorderPane.widthProperty().addListener(
                 obs -> setImageViewWidth((ObservableDoubleValue) obs));
-        codeChartsPane.heightProperty().addListener(
+        codeChartsBorderPane.heightProperty().addListener(
                 obs -> setImageViewHeight((ObservableDoubleValue) obs));
+        setGridPane();
     }
 }
