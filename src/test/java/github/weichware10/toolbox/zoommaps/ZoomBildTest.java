f70bb9ec5a80f76894c12204ab6b496a50a4b7aa
TheDarkDesync <philipmk69@gmail.com>
2022-01-08 12:54:06 +0100

    [test] Exceptions

--- src/test/java/github/weichware10/toolbox/zoommaps/ZoomBildTest.java
+++ src/test/java/github/weichware10/toolbox/zoommaps/ZoomBildTest.java
@@ -1,5 +1,8 @@
 package github.weichware10.toolbox.zoommaps;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.MalformedURLException;
 import javafx.geometry.Point3D;
 import javafx.scene.image.ImageView;
 import org.junit.Ignore;
@@ -11,11 +14,17 @@ import org.junit.Test;
 public class ZoomBildTest {
     /**
      * Testet ob ein Array mit der richtigen Länge erstellt wird.
+     *
+     * @throws IOException -
+     * @throws FileNotFoundException -
+     * @throws IllegalArgumentException -
+     * @throws MalformedURLException -
      */
     @Test
     @SuppressWarnings("unused")
     @Ignore
-    public void testOutputSize() {
+    public void testOutputSize() throws MalformedURLException, IllegalArgumentException,
+            FileNotFoundException, IOException {
         String location = ZoomBildTest.class.getResource("owl.jpeg").toString();
         ZoomBild zoombild = new ZoomBild(location, new ImageView(), null);
         Point3D position = new Point3D(0, 1, 2);
@@ -24,9 +33,15 @@ public class ZoomBildTest {
 
     /**
      * Testet, ob falsch übergebener String einen Error throwt.
+     *
+     * @throws IOException -
+     * @throws FileNotFoundException -
+     * @throws IllegalArgumentException -
+     * @throws MalformedURLException -
      */
     @Test(expected = IllegalArgumentException.class) // erwartet, dass Fehler auftritt
-    public void shouldThrowAtWrongPicture() {
+    public void shouldThrowAtWrongPicture() throws MalformedURLException, IllegalArgumentException,
+            FileNotFoundException, IOException {
         new ZoomBild("www.thisisnotapicture.com/owl.html", new ImageView(), null);
     }
 }
